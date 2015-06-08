88f9c5aaa7a5ac71a9d5a27d8d44ecee16b0c7b9
unknown <alee226@PACCPL-FMQ1TM1.cable.comcast.com>
2015-06-08 15:52:21 -0400

    new changes

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
+var http_IP = '127.0.0.1';
+var http_port = 8899;
 var http = require('http');
+var server = http.createServer(function(req, res) {
+  require('./router').get(req, res);
+}); //end server
 
-http.createServer(function(request, response) {
-
-  response.writeHead(200, {
-  	'Content-Type': 'text/html'
-  });
-  response.write('<!doctype html>\n<html lang="en">\n' + 
-    '\n<meta charset="utf-8">\n<title>Test web page on node.js</title>\n' + 
-    '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' + 
-    '\n\n<h1>Euro 2012 teams</h1>\n' + 
-    '<div id="content"><p>The teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' + 
-    '\n\n');
-  response.end();
-
-
-}).listen(8888);
-
-  console.log('Server running');
+server.listen(http_port, http_IP);
+console.log('listening to http://' + http_IP + ":" + http_port);
