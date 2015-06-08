9638346ff529b63bda86ddcb019f3f3cd5838de5
unknown <alee226@PACCPL-FMQ1TM1.cable.comcast.com>
2015-06-08 15:07:34 -0400

    this is a new commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require('http');
+
+http.createServer(function(request, response) {
+
+  response.writeHead(200, {
+  	'Content-Type': 'text/html'
+  });
+  response.write('<!doctype html>\n<html lang="en">\n' + 
+    '\n<meta charset="utf-8">\n<title>Test web page on node.js</title>\n' + 
+    '<style type="text/css">* {font-family:arial, sans-serif;}</style>\n' + 
+    '\n\n<h1>Euro 2012 teams</h1>\n' + 
+    '<div id="content"><p>The teams in Group D for Euro 2012 are:</p><ul><li>England</li><li>France</li><li>Sweden</li><li>Ukraine</li></ul></div>' + 
+    '\n\n');
+  response.end();
+
+
+}).listen(8888);
+
+  console.log('Server running');
